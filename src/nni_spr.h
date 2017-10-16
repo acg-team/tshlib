bfcc884880bffe631be6af56b937d222119c8fb5
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2017-10-16 11:18:45 +0200

    #ADD: Added nni_spr.h header file (it must be restructured to be a class)
    Signed-off-by: Lorenzo Gatti <lorenzo.gatti.89@gmail.com>

---
+++ src/nni_spr.h
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Licensed Materials - Property of Lorenzo Gatti & Massimo Maiolo
+ *
+ *
+ * Copyright (C) 2015-2017 by Lorenzo Gatti & Massimo Maiolo
+ *******************************************************************************
+ *
+ * This file is part of tshexe
+ *
+ * tshexe is a free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tshexe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with likpip. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
+/**
+ * @file nni_spr.h
+ * @author Lorenzo Gatti & Massimo Maiolo
+ * @date 16 10 2017
+ * @version 1.0
+ * @maintainer Lorenzo Gatti
+ * @email lg@lorenzogatti.me
+ * @status Development
+ *
+ * @brief
+ * @details
+ * @pre
+ * @bug
+ * @warning
+ *
+ * @see For more information visit: http://www.lorenzogatti.me
+ */
+#ifndef TSHLIB_NNI_SPR_H
+#define TSHLIB_NNI_SPR_H
+
+#include "PhyTree.hpp"
+
+struct move_info
+{
+    PhyTree *node1;
+    PhyTree *node2;
+    int ID;
+    double lk;
+};
+
+
+#endif //TSHLIB_NNI_SPR_H
