310ea9436c4febd28a5ead4d7db3cd731e73aad0
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2017-10-20 12:00:22 +0200

    #ADD: Added documentation to TreeRearrangement file
    Signed-off-by: Lorenzo Gatti <lorenzo.gatti.89@gmail.com>

--- src/TreeRearrangment.hpp
+++ src/TreeRearrangment.hpp
@@ -59,49 +59,82 @@ class Move {
 private:
 
 public:
-
+    /*!
+     * @brief Standard constructor
+     */
     Move();
 
+    /*!
+    * @brief Standard deconstructor
+    */
     ~Move();
 
+    /*!
+     * @brief Set the protected move_targetnode field
+     * @param target_node PhyTree Pointer to the target node
+     */
     void setTargetNode(PhyTree *target_node);
 
+    /*!
+     * @brief Reset the protected move_targetnode field
+     */
     void deleteTargetNode();
 
 protected:
 
-    int move_id;
-    std::string move_name;
-    std::string move_desc;
-    PhyTree *move_targetnode;
-    double move_lk;
-    bool move_applied;
-    std::string move_class;
+    int move_id;                    /* Move ID - Useful in case of parallel independent executions*/
+    std::string move_name;          /* Move Name - Unused */
+    std::string move_desc;          /* Move Desc - Unused */
+    PhyTree *move_targetnode;       /* Pointer to the target node found during the node search */
+    double move_lk;                 /* Likelihood of the move if applied */
+    bool move_applied;              /* Indicator is set to true if the move is applied to the tree */
+    std::string move_class;         /* Move class (i.e. NNI,SPR,TBR) - Usefull in case of mixed tree-search strategies */
 
 };
 
 class TreeRearrangment {
 
 private:
-    PhyTree *mset_sourcenode;
-    std::string mset_id;
-    int mset_radius;
-    bool mset_preserve_blenghts;
-    std::vector<Move *> mset_moves;
-    std::string mset_strategy;
+    PhyTree *mset_sourcenode;       /* Starting node from which starting the tree exploration */
+    std::string mset_id;            /* Tree-rearrangment ID. Useful in case of parallel independent executions */
+    int mset_radius;                /* Radius of the node search (for NNI must set it to 1) */
+    bool mset_preserve_blenghts;    /* Switch to preserve branch lentghs in case the move is applied (i.e NNI vs SPR) */
+    std::vector<Move *> mset_moves; /* Vector containing the pre-computed moves */
+    std::string mset_strategy;      /* Description of the node search strategy */
 
+    /*!
+     * @brief Recursive function to retrieve all the nodes within a fixed radius from a starting node
+     * @param node_source PhyTree Pointer to the starting node
+     * @param radius int Radius of the search (NNI = 1, SPR > 1)
+     * @param save bool ?
+     */
     void getNodesInRadius(PhyTree *node_source, int radius, bool save);
 
+    /*!
+     * @brief Recursive function to retrieve all the nodes within a fixed radius from a starting node
+     * @param node_source   PhyTree Pointer to the starting node
+     * @param radius        int Radius of the search (NNI = 1, SPR > 1)
+     * @param direction     int ?
+     */
     void getNodesInRadiusUp(PhyTree *node_source, int radius, int direction);
 
+    /*!
+     * @brief Append candidate move to the mset_moves vector
+     * @param move Move Pointer to the candidate move object
+     */
     void addMove(Move *move);
 
 
 public:
+
     TreeRearrangment(PhyTree *node_source, int radius, bool preserve_blengths);
 
     ~TreeRearrangment();
 
+    /*!
+     * @brief Perform a complete node search and fill the vector containing the candidate moves.
+     * @param saveMove bool ?
+     */
     void fillListMoves(bool saveMove);
 
 protected:
