bb90358f45e852c712fad5ee16d48ff79a147495
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2017-12-12 11:18:19 +0100

    #REF: Solved includes
    Signed-off-by: Lorenzo Gatti lorenzo.gatti.89@gmail.com <lorenzo.gatti.89@gmail.com>

--- main.cpp
+++ main.cpp
@@ -48,10 +48,8 @@
 #include <iomanip>
 #include <glog/logging.h>
 #include <gflags/gflags.h>
-//#define LOGURU_IMPLEMENTATION 1
-//#define LOGURU_WITH_STREAMS 1
 
-//#include <loguru.hpp>
+
 #include <Utree.hpp>
 #include <TreeRearrangment.hpp>
 #include <Likelihood.hpp>
